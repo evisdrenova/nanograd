e226a30e9913769c991bd3342c2dfe0e44cac957
Evis Drenova <evis@usenucleus.cloud>
2025-11-09 20:13:45 -0800

    feat(tensor): add add function and clone implementation for Tensor

--- src/tensor.rs
+++ src/tensor.rs
@@ -43,4 +43,42 @@ impl Tensor {
             })),
         }
     }
+
+    pub fn add(self, other: &Tensor) -> Tensor {
+        // compute forward pass data operation
+        let data = self.data() + other.data();
+
+        // create clones of the data that we can hand off to our backward_fn
+        let self_clone = self.clone();
+        let other_clone = other.clone();
+
+        // create the output tensor first and we'll fill in the backward_fn next
+        let output = Tensor::from_op(data, vec![self.clone(), other.clone()], Box::new(|| {}));
+
+        // clone output for backward function
+        let output_clone = output.clone();
+
+        // define the backward func and box it on the heap to run later
+        let backward_fn = Box::new(move || {
+            // gradient from output (c)
+            let output_grad = output_clone.grad();
+
+            // use += to accumulate bc we might run this multiple times
+            self_clone.inner.lock().unwrap().grad += output_grad;
+            other_clone.inner.lock().unwrap().grad += output_grad;
+        });
+
+        // update the backward func
+        output.inner.lock().unwrap().backward_fn = Some(backward_fn);
+
+        output
+    }
+}
+
+impl Clone for Tensor {
+    fn clone(&self) -> Tensor {
+        Tensor {
+            inner: Arc::clone(&self.inner),
+        }
+    }
 }
