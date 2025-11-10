71b7daa43f344bac1af35d8a964f47b5c6def5f8
Evis Drenova <evis@usenucleus.cloud>
2025-11-09 17:38:25 -0800

    feat(tensor.rs): add backward functionality to Tensor struct

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,8 +1,12 @@
 use std::sync::{Arc, Mutex};
 
+type BackwardFn = Box<dyn Fn() + Send>;
+
 struct TensorData {
     data: f64,
     grad: f64,
+    prev: Vec<Tensor>,
+    backward_fn: Option<BackwardFn>,
 }
 
 pub struct Tensor {
@@ -12,7 +16,12 @@ pub struct Tensor {
 impl Tensor {
     pub fn new(data: f64) -> Self {
         Tensor {
-            inner: Arc::new(Mutex::new(TensorData { data, grad: 0.0 })),
+            inner: Arc::new(Mutex::new(TensorData {
+                data,
+                grad: 0.0,
+                prev: Vec::new(),
+                backward_fn: None,
+            })),
         }
     }
 
@@ -22,5 +31,16 @@ impl Tensor {
 
     pub fn grad(&self) -> f64 {
         self.inner.lock().unwrap().grad
+    }
+
+    fn from_op(data: f64, prev: Vec<Tensor>, backward_fn: BackwardFn) -> Self {
+        Tensor {
+            inner: Arc::new(Mutex::new(TensorData {
+                data,
+                grad: 0.0,
+                prev,
+                backward_fn: Some(backward_fn),
+            })),
+        }
     }
 }
